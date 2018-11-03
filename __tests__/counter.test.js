1ff86600bbc1891731f61cbfbad467aa2085da42
Miles <14067660+milesflo@users.noreply.github.com>
2018-11-02 17:56:07 -0700

    Adding undefined state case

--- __tests__/counter.test.js
+++ __tests__/counter.test.js
@@ -12,3 +12,12 @@ test('counter reducer function DEC', () => {
   expect(counter(1, {type: DEC})).toBe(0)
   expect(counter(2, {type: DEC})).toBe(1)
 })
+
+test('counter reducer function OTHER', () => {
+  expect(counter(1, { type: 'THING' })).toBe(1)
+  expect(counter(2, { type: 'ANOTHER_THING' })).toBe(2)
+})
+
+test('counter reducer function UNDEFINED_STATE', () => {
+  expect(counter(undefined, { type: 'THING' })).toBe(0)
+})
--- counter.js
+++ counter.js
@@ -4,7 +4,10 @@ const actions = {
 }
 
 
-function counter(state, action){
+function counter(state, action) {
+  if (typeof state === 'undefined') {
+    return 0
+  }
   if (action.type === actions.INC) {
     return state + 1;
   }
